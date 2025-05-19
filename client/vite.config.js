e08bac0a4efd19a79353cf917b01f9491d806171
Vaishnavii621 <136267681+Vaishnavii621@users.noreply.github.com>
2025-05-19 16:36:10 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   build: {
+//     outDir: 'dist', // This is the default; ensure this matches your output directory
+//   }
+// });
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist', // This is the default; ensure this matches your output directory
+    outDir: 'dist',
   }
 });
